9aa0ec75ab28e3b93de8d9c66d21187a654a8208
ajinbramantio <ajinbramantio@gmail.com>
2019-02-01 10:26:46 +0700

    create javascript calculator

---
+++ index.js
@@ -0,0 +1,22 @@
+function add(number1, number2) {
+  return number1 + number2
+}
+
+function substract(number1, number2) {
+  return number1 - number2
+}
+function multiply(number1, number2) {
+  return number1 * number2
+}
+function divide(number1, number2) {
+  return number1 / number2
+}
+function modulus(number1, number2) {
+  return number1 % number2
+}
+
+console.log(modulus(6, 3))
+console.log(add(12, 3))
+console.log(divide(9, 3))
+console.log(multiply(3, 3))
+console.log(substract(16, 3))
